027b1d78927f7194bdda8119aa1b488118126f84
tinyEnd <41897529+tinyEnd@users.noreply.github.com>
2018-11-23 13:04:58 +0800

    添加气泡图

---
+++ client/app/visualizations/echarts/bubble/bubble-utils.js
@@ -0,0 +1,118 @@
+import { each, uniq, max } from 'lodash';
+
+
+function ChartDataHelper() {
+  let xValueColumn;
+  let yValueColumns;
+  let queryResult;
+  let groupingColumn;
+  let namesColumn;
+
+  ChartDataHelper.prototype.init = (result, xAxis, yAxis, groupColumn, nameColumn) => {
+    xValueColumn = xAxis;
+    groupingColumn = groupColumn;
+    yValueColumns = yAxis;
+    queryResult = result;
+    namesColumn = nameColumn;
+  };
+
+  ChartDataHelper.prototype.getGroupingData = () => {
+    const data = [];
+    each(queryResult, (item) => {
+      data.push(item[groupingColumn]);
+    });
+    return uniq(data);
+  };
+
+  ChartDataHelper.prototype.getBubbleData = () => {
+    if (groupingColumn) {
+      const allGroupData = [];
+      each(ChartDataHelper.prototype.getGroupingData(), (groupItem) => {
+        const oneGroupData = [];
+        each(queryResult, (item) => {
+          if (item[groupingColumn] === groupItem) {
+            oneGroupData.push([item[xValueColumn], item[yValueColumns], item[namesColumn], item[groupingColumn]]);
+          }
+        });
+        allGroupData.push(oneGroupData);
+      });
+      return allGroupData;
+    }
+    const allData = [[]];
+    each(queryResult, (item) => {
+      allData[0].push([item[xValueColumn], item[yValueColumns], item[namesColumn]]);
+    });
+    return allData;
+  };
+}
+
+// 获取值列中的最大值，用于计算气泡大小
+function getMaxValue(bubbleData) {
+  const valueList = [];
+  each(bubbleData, (item) => {
+    each(item, (value) => {
+      valueList.push(value[0]);
+    });
+  });
+  return max(valueList);
+}
+
+function BubbleOption() {
+  this.chartOption = {
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        show: true,
+        type: 'cross',
+      },
+    },
+    legend: {
+      show: true,
+      x: 'right',
+      y: 'top',
+      orient: 'vertical',
+    },
+    xAxis: {},
+    yAxis: {},
+    series: [{
+      data: [],
+      type: 'scatter',
+    }],
+  };
+
+  this.chartHelper = new ChartDataHelper();
+
+  this.setLegend = (legend) => {
+    this.chartOption.legend.data = legend ? this.chartHelper.getGroupingData() : [];
+  };
+
+  this.setBubbleData = () => {
+    each(this.chartHelper.getBubbleData(), (item, index) => {
+      this.chartOption.series[index] = {
+        name: this.chartHelper.getGroupingData()[index],
+        data: item,
+        itemStyle: {
+          opacity: 0.6,
+        },
+        type: 'scatter',
+        // 此回调函数将生成不同大小的气泡
+        symbolSize: (data) => {
+          const maxValue = getMaxValue(this.chartHelper.getBubbleData());
+          if (data[0] / (maxValue / 50) < 5) {
+            return 5;
+          }
+          return data[0] / (maxValue / 50);
+        },
+        label: {
+          emphasis: {
+            show: true,
+            formatter: param => param.data[2],
+            position: 'top',
+          },
+        },
+      };
+    });
+  };
+}
+
+export { BubbleOption, ChartDataHelper };
---
+++ client/app/visualizations/echarts/bubble/index.js
@@ -0,0 +1,90 @@
+import _ from 'lodash';
+import EchartsFactory from '@/lib/visualizations/echarts/echarts-factory';
+import { BubbleOption } from './bubble-utils';
+import editorTemplate from './bubble-editor.html';
+
+
+function BubbleRenderer($location, currentUser) {
+  return {
+    restrict: 'E',
+    template: '<div class="echarts-chart-visualization-container" resize-event="handleResize()"></div>',
+    link($scope, element) {
+      const container = element[0].querySelector('.echarts-chart-visualization-container');
+      const echartFactory = new EchartsFactory($location, currentUser);
+      const bubbleChart = echartFactory.createChart(container);
+
+
+      function reloadData() {
+        const data = $scope.queryResult.getData();
+        const editOptions = $scope.visualization.options.editOptions;
+        const bubbleExamples = new BubbleOption();
+
+        if (!editOptions.xValueColumn) {
+          echartFactory.setOption(bubbleChart, (new BubbleOption()).chartOption);
+          return;
+        }
+
+        bubbleExamples.chartOption.xValueColumn = editOptions.xValueColumn;
+        bubbleExamples.chartOption.yValueColumn = editOptions.yValueColumn;
+        bubbleExamples.chartOption.nameColumn = editOptions.nameColumns;
+        bubbleExamples.chartOption.result = data;
+        bubbleExamples.chartOption.groupByColumn = editOptions.groupBy;
+        bubbleExamples.chartHelper
+          .init(data, editOptions.xValueColumn, editOptions
+            .yValueColumn, editOptions.groupBy, editOptions.nameColumns);
+        bubbleExamples.setLegend(editOptions.legend);
+        bubbleExamples.chartOption.yAxis.max = editOptions.yAxisMax;
+        bubbleExamples.chartOption.yAxis.min = editOptions.yAxisMin;
+        bubbleExamples.chartOption.xAxis.max = editOptions.xAxisMax;
+        bubbleExamples.chartOption.xAxis.min = editOptions.xAxisMin;
+        bubbleExamples.chartOption.xAxis.name = editOptions.xName;
+        bubbleExamples.chartOption.yAxis.name = editOptions.yName;
+        bubbleExamples.setBubbleData();
+
+        echartFactory.setOption(bubbleChart, bubbleExamples.chartOption);
+      }
+
+      function resize() {
+        window.onresize = bubbleChart.resize;
+      }
+
+      $scope.handleResize = _.debounce(resize, 50);
+      $scope.$watch('visualization.options', reloadData, true);
+      $scope.$watch('queryResult && queryResult.getData()', reloadData);
+    },
+  };
+}
+
+function BubbleEditor() {
+  return {
+    restrict: 'E',
+    template: editorTemplate,
+    link($scope) {
+      $scope.currentTab = 'general';
+      $scope.changeTab = (tab) => {
+        $scope.currentTab = tab;
+      };
+      const editOptions = {
+        legend: true,
+      };
+      if (!$scope.visualization.id) $scope.visualization.options.editOptions = editOptions;
+    },
+  };
+}
+
+
+export default function init(ngModule) {
+  ngModule.directive('bubbleRenderer', BubbleRenderer);
+  ngModule.directive('bubbleEditor', BubbleEditor);
+  ngModule.config((VisualizationProvider) => {
+    const renderTemplate = '<bubble-renderer options="visualization.options" query-result="queryResult"></bubble-renderer>';
+    const editTemplate = '<bubble-editor options="visualization.options" query-result="queryResult"></bubble-editor>';
+
+    VisualizationProvider.registerVisualization({
+      type: 'echart-bubble',
+      name: '气泡图',
+      renderTemplate,
+      editorTemplate: editTemplate,
+    });
+  });
+}
