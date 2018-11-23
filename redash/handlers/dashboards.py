2f27a78f2ad27bc07238ec8b24d753570993cf4c
tyrozhang <tyroz@qq.com>
2018-11-23 09:54:59 +0800

    应共享出去的dashboard出现权限方面的问题，导致新开浏览器不能访问，暂时恢复原功能

--- redash/handlers/dashboards.py
+++ redash/handlers/dashboards.py
@@ -248,8 +248,8 @@ class PublicDashboardResource(BaseResource):
         else:
             dashboard = self.current_user.object
 
-        # return serializers.public_dashboard(dashboard)
-        return serializers.serialize_dashboard(dashboard, with_widgets=True, user=self.current_user)
+        return serializers.public_dashboard(dashboard)
+        # return serializers.serialize_dashboard(dashboard, with_widgets=True, user=self.current_user)
 
 
 class DashboardShareResource(BaseResource):
