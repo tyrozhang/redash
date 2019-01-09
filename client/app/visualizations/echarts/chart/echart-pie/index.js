8f3e3744e202a6f80d37d2f58cf6a6b0c5b1bfa9
tinyEnd <41897529+tinyEnd@users.noreply.github.com>
2019-01-09 13:36:09 +0800

    将饼图的utils独立

--- client/app/visualizations/echarts/chart/echart-pie/index.js
+++ client/app/visualizations/echarts/chart/echart-pie/index.js
@@ -1,8 +1,8 @@
 import _ from 'lodash';
 import EchartsFactory from '@/lib/visualizations/echarts/echarts-factory';
-import { PieOption } from '@/visualizations/echarts/chart/utils';
 import { dataDrilling, getHasFiltersDashboards } from '@/visualizations/echarts/chart/data-drilling/util';
 import chartIcon from '@/assets/images/visualizationIcons/icon_pie.png';
+import PieOption from './pie-utils';
 import editorTemplate from './pie-editor.html';
 
 
